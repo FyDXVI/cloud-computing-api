a9aba47ad0df2bd18595d79a3590c9661bdec8ad
Justin Martin <justin.martin@student.junia.com>
2024-11-23 22:07:17 +0100

    feat(vnet-ressource_group): add comments

--- infrastructure/modules/resource_group/main.tf
+++ infrastructure/modules/resource_group/main.tf
@@ -1,3 +1,5 @@
+## Resource Group
+# This resource creates a resource group in Azure.
 resource "azurerm_resource_group" "resource_group" {
   name     = var.rg_name
   location = var.physical_loc
--- infrastructure/modules/vnet/main.tf
+++ infrastructure/modules/vnet/main.tf
@@ -1,4 +1,6 @@
-## Virtual network
+## Virtual Network
+# This resource creates a virtual network (VNet) in Azure.
+# The VNet defines a private network space for your Azure resources to communicate.
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = var.physical_loc
@@ -7,6 +9,8 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 ## Subnets
+# This resource configures subnets within the VNet.
+# Each subnet can have specific address ranges and associated services (e.g., Azure Storage).
 resource "azurerm_subnet" "subnets" {
   for_each             = { for subnet in var.subnets : subnet.name => subnet }
   name                 = each.value.name
@@ -29,5 +33,4 @@ resource "azurerm_subnet" "subnets" {
       }
     }
   }
-
 }
